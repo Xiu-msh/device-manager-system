5e08622072ba103e46695a180bf4358b28f516c4
shin <18309182914@163.com>
2025-06-14 17:31:46 +0800

    update debug false

--- device_manager/settings.py
+++ device_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cyg9j3mteqjqvx2+gtn5&19wf*hqc-y4p%keuh7hup$(%febu&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 
